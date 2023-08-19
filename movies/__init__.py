f764cfd3b9bd5771ba2cc5519f6170621daa5d9b
MMurtadhaM <106926315+MMurtadhaM@users.noreply.github.com>
2023-08-19 11:30:09 +0300

    commet #1

---
+++ movies/__init__.py
@@ -0,0 +1,22 @@
+"""
+super user: murtadha 88888888
+
+
+add movies app and rest-framework app to the INSTALLED_APPS
+
+create the model and add the str method
+
+confirme the admin to appear the model data
+
+create serializers file and make the serializer class
+
+create the view to the serializer and connect the url
+
+.....
+add type field to the model then migrate it
+
+write views for action and comedy 
+convert the type of router from default to simple and register the new views
+
+images
+"""
